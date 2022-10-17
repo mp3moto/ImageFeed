bdfe7fe6b473f0cd76843d266f4e9d5148ffff9b
mp3moto <mp3moto@mail.ru>
2022-10-17 22:30:04 +0500

    segues work

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image = UIImage()
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.image = image
+    }
+}
--- ImagesList/ImagesListViewController.swift
+++ ImagesList/ImagesListViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     @IBOutlet private var tableView: UITableView!
     private var photos: [String] = []
 
@@ -45,11 +46,22 @@ extension ImagesListViewController: UITableViewDataSource {
         return imageListCell
     }
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            let viewController = segue.destination as! SingleImageViewController
+            let indexPath = sender as! IndexPath
+            if let image = UIImage(named: photos[indexPath.row]) {
+                viewController.image = image
+            }
+        }
+        else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
 }
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
     }
 }

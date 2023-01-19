ba34a94275c94ba6a366b8367996ca406d8d4e3e
Renat Gareev <mp3moto@mail.ru>
2023-01-19 07:23:54 +0500

    review_2.02

--- ImagesList/ImagesListCell.swift
+++ ImagesList/ImagesListCell.swift
@@ -18,27 +18,6 @@ final class ImagesListCell: UITableViewCell {
     override func prepareForReuse() {
         super.prepareForReuse()
         
-        let gradient = CAGradientLayer()
-        gradient.frame = CGRect(origin: .zero, size: CGSize(width: cellImage.bounds.width, height: cellImage.bounds.height))
-        gradient.locations = [0, 0.1, 0.3]
-        gradient.colors = [
-            UIColor(red: 0.682, green: 0.686, blue: 0.706, alpha: 1).cgColor,
-            UIColor(red: 0.531, green: 0.533, blue: 0.553, alpha: 1).cgColor,
-            UIColor(red: 0.431, green: 0.433, blue: 0.453, alpha: 1).cgColor
-        ]
-        gradient.startPoint = CGPoint(x: 0, y: 0.5)
-        gradient.endPoint = CGPoint(x: 1, y: 0.5)
-        gradient.cornerRadius = 16
-        gradient.masksToBounds = true
-        
-        let gradientChangeAnimation = CABasicAnimation(keyPath: "locations")
-        gradientChangeAnimation.duration = 1.0
-        gradientChangeAnimation.autoreverses = true
-        gradientChangeAnimation.repeatCount = .infinity
-        gradientChangeAnimation.fromValue = [0, 0.1, 0.3]
-        gradientChangeAnimation.toValue = [0, 0.8, 1]
-        gradient.add(gradientChangeAnimation, forKey: "locationsChange")
-        
         cellImage.kf.cancelDownloadTask()
         cellDate.text = ""
         cellLike.setImage(UIImage(named: "NoLike"), for: .normal)
